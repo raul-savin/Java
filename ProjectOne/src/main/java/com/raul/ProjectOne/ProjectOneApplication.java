85a464c6e4129a637764d4400be37743212e221a
webdevraul <webdevraul.github@gmail.com>
2024-10-02 03:38:34 +0100

    chore(ProjectOne): add @Log to ProjectOneApplication!

--- ProjectOne/src/main/java/com/raul/ProjectOne/ProjectOneApplication.java
+++ ProjectOne/src/main/java/com/raul/ProjectOne/ProjectOneApplication.java
@@ -1,9 +1,11 @@
 package com.raul.ProjectOne;
 
+import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log
 public class ProjectOneApplication {
 
 	public static void main(String[] args) {
