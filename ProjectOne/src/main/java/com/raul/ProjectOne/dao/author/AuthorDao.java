882c262c99d5190eae965e52f697f6d1b5c0e18a
webdevraul <webdevraul.github@gmail.com>
2024-10-03 08:41:00 +0100

    feat(projectOne): add findOne & AuthorRowMapper methods for AuthorDao!

--- ProjectOne/src/main/java/com/raul/ProjectOne/dao/author/AuthorDao.java
+++ ProjectOne/src/main/java/com/raul/ProjectOne/dao/author/AuthorDao.java
@@ -1,10 +1,19 @@
 package com.raul.ProjectOne.dao.author;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import com.raul.ProjectOne.dao.types.author.AuthorInterface;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.raul.ProjectOne.dao.types.author.AuthorDaoInterface;
 import com.raul.ProjectOne.domain.author.Author;
 
-public class AuthorDao implements AuthorInterface {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class AuthorDao implements AuthorDaoInterface {
 
   private final JdbcTemplate jdbcTemplate;
 
@@ -17,6 +26,27 @@ public class AuthorDao implements AuthorInterface {
     jdbcTemplate.update(
         "INSERT INTO authors (id, name, age) VALUES ($1, $2, $3)",
         author.getId(), author.getName(), author.getAge());
+  }
+
+  @Override
+  public Optional<Author> findOne(long authorId) {
+    List<Author> results = jdbcTemplate.query(
+        "SELECT id, name, age FROM authors WHERE id = $1 LIMIT 1",
+        new AuthorRowMapper(), authorId);
+
+    return results.stream().findFirst();
+  }
+
+  public static class AuthorRowMapper implements RowMapper<Author> {
+
+    @Override
+    public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
+      return Author.builder()
+          .id(rs.getLong("id"))
+          .name(rs.getString("name"))
+          .age(rs.getInt("age"))
+          .build();
+    }
   }
 
 }
