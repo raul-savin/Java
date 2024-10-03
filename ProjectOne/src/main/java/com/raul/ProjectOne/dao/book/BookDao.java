13c6bb56c7f9e35c546ffb8dc37d808ddcc1e826
webdevraul <webdevraul.github@gmail.com>
2024-10-03 08:42:46 +0100

    feat(projectOne): add find & BookRowMapper methods for BookDao!

--- ProjectOne/src/main/java/com/raul/ProjectOne/dao/book/BookDao.java
+++ ProjectOne/src/main/java/com/raul/ProjectOne/dao/book/BookDao.java
@@ -1,10 +1,19 @@
 package com.raul.ProjectOne.dao.book;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import com.raul.ProjectOne.dao.types.book.BookInterface;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.raul.ProjectOne.dao.types.book.BookDaoInterface;
 import com.raul.ProjectOne.domain.book.Book;
 
-public class BookDao implements BookInterface {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookDao implements BookDaoInterface {
 
   private final JdbcTemplate jdbcTemplate;
 
@@ -15,8 +24,31 @@ public class BookDao implements BookInterface {
   @Override
   public void create(Book book) {
     jdbcTemplate.update(
-        "INSERT INTO authors (isbn, title, author_id) VALUES ($1, $2, $3)",
-        book.getIsbn(), book.getTitle(), book.getAuthorId());
+        "INSERT INTO books (isbn, title, author_id) VALUES ($1, $2, $3)",
+        book.getIsbn(),
+        book.getTitle(),
+        book.getAuthorId());
   }
 
+  @Override
+  public Optional<Book> find(String isbn) {
+    List<Book> results = jdbcTemplate.query(
+        "SELECT isbn, title, author_id from books WHERE isbn = $1 LIMIT 1",
+        new BookRowMapper(),
+        isbn);
+    return results.stream().findFirst();
+  }
+
+  public static class BookRowMapper implements RowMapper<Book> {
+
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+      return Book.builder()
+          .isbn(rs.getString("isbn"))
+          .title(rs.getString("title"))
+          .authorId(rs.getLong("author_id"))
+          .build();
+    }
+
+  }
 }
